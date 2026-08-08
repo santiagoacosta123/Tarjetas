9aa48fc1d3567cc1b35407fa6d455d77cdcf6f4f
Santiago Acosta <santiagobarrera2004s@gmail.com>
2026-08-08 11:15:12 -0500

    actualizar

--- src/app/direcciones/actualizar-direcciones/actualizar-direcciones.ts
+++ src/app/direcciones/actualizar-direcciones/actualizar-direcciones.ts
@@ -1,11 +1,60 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-actualizar-direcciones',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './actualizar-direcciones.html',
-  styleUrl: './actualizar-direcciones.css',
+  styleUrl: './actualizar-direcciones.css'
 })
 export class ActualizarDirecciones {
+
+  direccion = {
+    address_id: 0,
+    address: '',
+    district: '',
+    city_id: 0,
+    phone: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  actualizarDireccion() {
+
+    this.http.patch(
+      `https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/address?address_id=eq.${this.direccion.address_id}`,
+      {
+        address: this.direccion.address,
+        district: this.direccion.district,
+        city_id: this.direccion.city_id,
+        phone: this.direccion.phone
+      },
+      {
+        headers: {
+          apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+          Authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+          'Content-Type': 'application/json'
+        }
+      }
+    ).subscribe({
+
+      next: () => {
+
+        alert('Dirección actualizada correctamente');
+
+      },
+
+      error: (error) => {
+
+        console.log(error);
+
+        alert('No se pudo actualizar la dirección');
+
+      }
+
+    });
+
+  }
 
 }
--- src/app/direcciones/direcciones.ts
+++ src/app/direcciones/direcciones.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 
+import { ListarDirecciones } from './listar-direcciones/listar-direcciones';
+import { CrearDirecciones } from './crear-direcciones/crear-direcciones';
+import { ActualizarDirecciones } from './actualizar-direcciones/actualizar-direcciones';
+
 @Component({
   selector: 'app-direcciones',
-  imports: [],
+  imports: [
+    ListarDirecciones,
+    CrearDirecciones,
+    ActualizarDirecciones
+  ],
   templateUrl: './direcciones.html',
-  styleUrl: './direcciones.css',
+  styleUrl: './direcciones.css'
 })
 export class Direcciones {
+
+  vista = 'listar';
+
+  mostrarListar() {
+    this.vista = 'listar';
+  }
+
+  mostrarCrear() {
+    this.vista = 'crear';
+  }
+
+  mostrarActualizar() {
+    this.vista = 'actualizar';
+  }
 
 }
